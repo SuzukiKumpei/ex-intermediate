2867706994a71217af1a57b4e79b61e95c673894
SuzukiKumpei <suzuki.kumpei@gmail.com>
2020-02-12 09:45:42 +0900

    ドメインDetailクラス作成完了

---
+++ src/main/java/com/example/domain/Detail.java
@@ -0,0 +1,68 @@
+package com.example.domain;
+
+/**
+ * 野球チーム詳細情報を表すドメイン.
+ * 
+ * @author suzukikunpei
+ *
+ */
+public class Detail {
+
+	/** 野球チームID */
+	private Integer id;
+	/** リーグ名 */
+	private String league_name;
+	/** 本拠地 */
+	private String headquarters;
+	/** 発足日 */
+	private String inauguration;
+	/** チーム変遷 */
+	private String history;
+
+	@Override
+	public String toString() {
+		return "Detail [id=" + id + ", league_name=" + league_name + ", headquarters=" + headquarters
+				+ ", inauguration=" + inauguration + ", history=" + history + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLeague_name() {
+		return league_name;
+	}
+
+	public void setLeague_name(String league_name) {
+		this.league_name = league_name;
+	}
+
+	public String getHeadquarters() {
+		return headquarters;
+	}
+
+	public void setHeadquarters(String headquarters) {
+		this.headquarters = headquarters;
+	}
+
+	public String getInauguration() {
+		return inauguration;
+	}
+
+	public void setInauguration(String inauguration) {
+		this.inauguration = inauguration;
+	}
+
+	public String getHistory() {
+		return history;
+	}
+
+	public void setHistory(String history) {
+		this.history = history;
+	}
+
+}
